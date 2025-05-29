4b7b6d5b869ee024a134c310c3283e04ef84ad01
sora-tt <sora.tomita@togawa.cs.waseda.ac.jp>
2025-05-30 00:33:16 +0900

    set database url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": db_url(config("DATABASE_URL"))
 }
 
 
